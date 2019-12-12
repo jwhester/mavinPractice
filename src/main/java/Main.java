9831200211625379a2b5c80b67140ad86f8a6cf6
jwhester <jameshester88@gmail.com>
2019-12-12 15:40:58 -0600

    finished

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,12 @@
+import org.apache.commons.lang3.StringUtils;
+import java.util.Scanner;
 public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Type what ever you want: ");
+        String newInput = input.nextLine();
+        System.out.println("Is it a number: " + StringUtils.isNumeric(newInput));
+        System.out.println("Swap that case: " + StringUtils.swapCase(newInput));
+        System.out.println("Reversed: " + StringUtils.reverse(newInput));
+    }
 }
